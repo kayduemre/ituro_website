19a254668d96b98902d050fb34ecad4b3f3a3721
kayduemre <emre.kyd11@gmail.com>
2019-02-23 01:53:06 +0300

    change url

--- ituro_website/survey/urls.py
+++ ituro_website/survey/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from survey.views import SurveyCreateView
 
 urlpatterns = [
-    path('<slug:slug>/$', SurveyCreateView.as_view(),
+    path('<slug:slug>/', SurveyCreateView.as_view(),
         name="survey_create"),
 ]
