746e368bd4dd1c3b2d4254a92df8f91c36b75bbc
kayduemre <emre.kyd11@gmail.com>
2019-02-23 01:52:36 +0300

    change url

--- ituro_website/post/urls.py
+++ ituro_website/post/urls.py
@@ -8,18 +8,18 @@ from post.views import HomePageDetailView, AboutDetailView, \
 
 urlpatterns = [
 
-    path('/$', HomePageDetailView.as_view(), name="homepage"),
-    path(_('contact_us/$'), contactView, name="contact"),
-    path(_('success/$'), successView, name="success"),
-    path(_('about/<slug:slug>/$'), AboutDetailView.as_view(),
+    path('', HomePageDetailView.as_view(), name="homepage"),
+    path(_('contact_us/'), contactView, name="contact"),
+    path(_('success/'), successView, name="success"),
+    path(_('about/<slug:slug>/'), AboutDetailView.as_view(),
         name="about_detail"),
-    path(_('category/(<slug:slug>/$'), CategoryDetailView.as_view(),
+    path(_('category/(<slug:slug>/'), CategoryDetailView.as_view(),
         name="category_detail"),
-    path(_('sponsorship/<slug:slug>/$'), SponsorshipDetailView.as_view(),
+    path(_('sponsorship/<slug:slug>/'), SponsorshipDetailView.as_view(),
         name="sponsorship_detail"),
 
-    path(_('news/<slug:slug>/$'), NewsEntryDetailView.as_view(),
+    path(_('news/<slug:slug>/'), NewsEntryDetailView.as_view(),
         name="news_detail"),
-    path('<slug:slug>/$', CommonDetailView.as_view(),
+    path('<slug:slug>/', CommonDetailView.as_view(),
         name="common_detail"),
 ]
