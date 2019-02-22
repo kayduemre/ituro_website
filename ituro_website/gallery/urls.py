3fcbff9a2639511ae0fe01476b5ac0ae4d71d115
kayduemre <emre.kyd11@gmail.com>
2019-02-23 00:50:51 +0300

    change url to path

---
+++ ituro_website/gallery/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from gallery.views import GalleryDetailView
+from django.urls import path
+
+urlpatterns = [
+
+    path('<slug:slug>/$', GalleryDetailView.as_view(),
+        name="gallery_detail"),
+]
